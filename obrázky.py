f0cfcea34fe0287509973adb99a31801f82ab441
Volldy <31956783+Volldy@users.noreply.github.com>
2017-11-01 19:53:00 +0100

    Add files via upload

---
+++ obrázky.py
@@ -0,0 +1,16 @@
+r = int(input('>>'))
+h = 1
+print()
+while r > 0:
+    print (r*' '+ h*'*')
+    h += 2
+    r += -1
+print()
+
+r = int(input('>>'))
+print()
+while r > 0:
+    print(r*'*')
+    r += -1
+print()
+
